2fbac1772ec1c32d4d22eafdc0fa7727f3a5d2b1
Hunter Warren <hunter.warren@gmail.com>
2017-07-19 14:28:11 -0500

    Added a guess counter.

--- high-low.php
+++ high-low.php
@@ -8,18 +8,24 @@ fwrite(STDOUT, 'Hey sucker. Guess a number! ');
 //compare input to number
 $number = trim(fgets(STDIN));
 
+//number of guesses
+$guesses = 0;
+
 while ($number != $rando) {
 
   if (is_numeric($number) == false) {
     fwrite(STDOUT, "You must enter a number!\n");
   } else if ($number < $rando) {
     fwrite(STDOUT, "Higher!\n");
+    $guesses +=1;
   } else if ($number > $rando) {
     fwrite(STDOUT, "Lower!\n");
+    $guesses +=1;
   }
     $number = trim(fgets(STDIN));
 }
 
 if($number == $rando) {
-  fwrite(STDOUT, "Did you cheat?!\nGAME OVER");
+  fwrite(STDOUT, "Did you cheat?!\nOnly took you {$guesses} guesses.\nGAME OVER");
+
 }
